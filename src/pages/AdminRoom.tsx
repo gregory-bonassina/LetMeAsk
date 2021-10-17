20ff852b59f30d7c1d3cbf53d9075a315db84b9c
Gregory Bonassina <bonassina3@gmail.com>
2021-10-17 17:29:55 -0300

    Implement questions structure; like function; admin page

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,78 @@
+import { useHistory, useParams } from 'react-router';
+
+import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
+
+import { database } from '../services/firebase';
+import { Button } from '../components/Button';
+import { Question } from '../components/Question';
+import { RoomCode } from '../components/RoomCode';
+import { useRoom } from '../hooks/useRoom';
+
+import '../styles/room.scss';
+
+type RoomParams = {
+    id: string;
+}
+
+export function AdminRoom() {
+    const history = useHistory();
+    const params = useParams<RoomParams>();
+    const roomId = params.id;
+
+    const { title, questions } = useRoom(roomId);
+
+    async function handlEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date(),
+        });
+
+        history.push('/');
+    }
+
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Tem certeza que deseja excluir essa pergunta?')) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+        }
+    }
+
+    return (
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="Letmeask" />
+                    <div>
+                        <RoomCode code={roomId}/>
+                        <Button isOutlined onClick={handlEndRoom}>Encerrar sala</Button>
+                    </div>
+                </div>
+            </header>
+
+            <main>
+                <div className="room-title">
+                    <h1>Sala {title}</h1>
+                    {questions.length > 0 && <span>{questions.length} Pergunta(s)</span>}
+                </div>
+
+                <div className="question-list">
+                    {questions.map(question => {
+                        return (
+                            <Question
+                                key={question.id}
+                                content={question.content}
+                                author={question.author}
+                            >
+                                <button
+                                    type="button"
+                                    onClick={() => handleDeleteQuestion(question.id)}
+                                >
+                                    <img src={deleteImg} alt="Remover pergunta" />
+                                </button>
+                            </Question>
+                        );
+                    })}
+                </div>
+            </main>
+        </div>
+    );
+}
